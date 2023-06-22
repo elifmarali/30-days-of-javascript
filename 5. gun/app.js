deef198dd726d48f89a80e081edcd84248703ba7
elifmarali <elifmarali00@gmail.com>
2023-06-22 16:16:13 +0300

    5. gun dersine baslandi

---
+++ 5. gun/app.js
@@ -0,0 +1,10 @@
+//array constructor i kullanarak dizi tanimlayalim 
+const arr = Array();
+console.log(arr);
+//ya da 
+const arr2 = new Array();
+console.log(arr2);
+
+//bos bir dizi olusturmak icin en cok tavsiye edilen yontem ise;
+const arr3 = [];
+console.log(arr3);

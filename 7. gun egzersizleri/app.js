241ec136b8bb0559d320cf428e408c58b393ec1e
elifmarali <elifmarali00@gmail.com>
2023-07-11 13:07:07 +0300

    foksiyonlar egzersizleri 1. level bitti 2. levela baslandi

--- 7. gun egzersizleri/app.js
+++ 7. gun egzersizleri/app.js
@@ -120,14 +120,112 @@ console.log(findMax(0, 10, 5))
 10
 console.log(findMax(0, -10, -2))
 0 */
-/* function findMax(num1, num2, num3) {
-    let max = 0;
-    let newArr = []
-    for (let i = 0; i < arguments.length; i++) {
-        if (max < arguments[i]) {
-            newArr.push(arguments[i])
-        }
+
+function findMax(num1, num2, num3) {
+    let max = num1;
+    let newNum1 = [];
+    if (max < num2) {
+        max = num2;
     }
-    return newArr;
+    if (max < num3) {
+        max = num3;
+    }
+    newNum1.push(max);
+    return newNum1;
 }
-console.log(findMax(5, 3, 9)); */
+console.log(findMax(3, 9, 1));
+
+// ekstra: daha zor bir halini yapalim 
+// dizi olarak alip num1, num2,num3 u siralayalim buyukten kucuge dizi olarak yazdiralim
+function funcSort(num1, num2, num3) {
+    let numm = [num1, num2, num3];
+    let newNum = [];
+    let num;
+    if (num1 < num2) {
+        let num = num1;
+        num1 = num2;
+        num2 = num;
+    }
+    if (num2 < num3) {
+        let num = num2;
+        num2 = num3;
+        num3 = num;
+    }
+    if (num1 < num3) {
+        let num = num1;
+        num1 = num3;
+        num3 = num;
+    }
+    newNum.push(num1, num2, num3);
+    return newNum;
+}
+console.log(funcSort(3, 7, 2));
+
+//level 2
+// 1. Linear equation is calculated as follows: ax + by + c = 0. Write a function which calculates value of a linear equation, solveLinEquation.
+
+function solveLinEquation(x, y) {
+    let a = 3, b = 2, c = 1;
+    let linearEquation = a * x + b * y + c;
+    console.log(linearEquation);
+}
+solveLinEquation(1, 2)
+
+/* 2. Quadratic equation is calculated as follows: ax2 + bx + c = 0. Write a function which calculates value or values of a quadratic equation, solveQuadEquation.
+
+console.log(solveQuadratic()) // {0}
+console.log(solveQuadratic(1, 4, 4)) // {-2}
+console.log(solveQuadratic(1, -1, -2)) // {2, -1}
+console.log(solveQuadratic(1, 7, 12)) // {-3, -4}
+console.log(solveQuadratic(1, 0, -4)) //{2, -2}
+console.log(solveQuadratic(1, -1, 0)) //{1, 0}
+
+ */
+
+let solveQuadratic = (a, b, c) => {
+    let result;
+    if (a === undefined || b === undefined || c === undefined) {
+        result = 0;
+        return result;
+    }
+    if (a === 0) {
+        result = "Girdiginiz a=0 degerinden dolayi bur bir ikinci dereceden denklem degildir Lütfen a katsayısını sıfırdan farklı bir değer olarak giriniz.";
+        return result
+    }
+    let discriminant = b * b - (4 * a * c);
+    if (discriminant < 0) {
+        result = "Gercek kok yoktur";
+    }
+    else if (discriminant === 0) {
+        let x = -b / (2 * a)
+        result = x
+    }
+    else {
+        let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        result = [x1, x2];
+    }
+    return result;
+}
+
+console.log(solveQuadratic())
+console.log(solveQuadratic(1, 4, 4))
+console.log(solveQuadratic(1, -1, -2))
+console.log(solveQuadratic(1, 7, 12))
+console.log(solveQuadratic(1, 0, -4))
+
+//3. Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+
+function printArray(arr) {
+    let result = "";
+    for (let i = 0; i < arr.length; i++) {
+        result += `${i}. index =  ${arr[i]}\n`
+    }
+    return result
+}
+console.log(printArray([1, 2, 7, 9, 12, 3]));
+
+// 4. Write a function name showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
+
+/* showDateTime()
+08 / 01 / 2020 04: 08 */
